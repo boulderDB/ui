ab6fd7bdd00bc0d46e5a66e2693196549ef7da98
schaschjan <jan-jakub@atelierdisko.de>
2021-12-09 17:23:35 +0100

    handle 404

--- hooks/useHttp.js
+++ hooks/useHttp.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useMemo } from "react";
 import useSWR from "swr";
+import { useRouter } from "next/router";
 
 const fetcher = (url, params) =>
   axios
@@ -11,9 +12,14 @@ const fetcher = (url, params) =>
 
 export function useCachedHttp(resource, params) {
   const { data, error } = useSWR(resource, () => fetcher(resource, params));
+  const router = useRouter();
 
   if (error) {
-    console.error(error);
+    if (error?.response.status === 404) {
+      return router.push("/404");
+    }
+
+    console.error(error?.response);
     throw error;
   }
 
