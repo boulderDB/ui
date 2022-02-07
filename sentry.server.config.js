64dcf0013d57cb3676c968d9251cc20a369bbc14
schaschjan <jan-jakub@atelierdisko.de>
2022-02-07 21:40:20 +0100

    fi import path

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,7 +3,7 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from "@sentry/nextjs";
-import { version } from "./../../package.json";
+import { version } from "./package.json";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
